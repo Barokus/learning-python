257e5069a58bd579aaac196d16a54fb3b045424d
ushakov-lebedinskiy.a <ushakov-lebedinskiy.a@msk.office.litres.ru>
2026-02-26 19:08:16 +0300

    добавлена история игр и сохранение в json

--- guess_number_v3.py
+++ guess_number_v3.py
@@ -1,11 +1,12 @@
+import json
 import random
 
 def play_game():
-    number_to_guess = random.randit(1,100)
+    number_to_guess = random.randint(1,100)
     attempts = []
 
     while True:
-        guess = int(input("Введите число от 1 до 100"))
+        guess = int(input("Введите число от 1 до 100: "))
         attempts.append(guess)
 
         if guess < number_to_guess:
@@ -26,10 +27,26 @@ def play_game():
 
     return result
 
+def save_result_to_file(result):
+    with open("game_result.json", "w", encoding="utf-8") as file:
+        json.dump(result, file, ensure_ascii=False, indent=4)
+
+def load_result_from_file():
+    with open("game_result.json", "r", encoding="utf-8") as file:
+        result = json.load(file)
+    return result
+
 def main():
     game_result = play_game()
+    save_result_to_file(game_result)    
 
-    print("Результат игры:")
+    print("Текущий результат: ")
     print(game_result)
+
+    print("\nРезультат из файла: ")
+    loaded_result = load_result_from_file()
+    print(loaded_result)
+
+    save_result_to_file(game_result)
     
 main()
